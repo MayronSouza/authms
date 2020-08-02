706cdaaa60dff1b808c489ce92cced07385f9da8
MayronSouza <mayron.souza@gmz.solutions>
2020-08-02 14:32:25 -0300

    Criando as pages SignIn e Dashboard do App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import 'react-native-gesture-handler'
 import React from 'react'
 import { View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
 
 const App: React.FC = () => {
-  return <View />
+  return (
+    <NavigationContainer>
+      <View />
+    </NavigationContainer>
+  )
 }
 
 export default App
